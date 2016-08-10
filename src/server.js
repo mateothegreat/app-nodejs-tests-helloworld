b395f4fa51da41de2f4907bafa3c81a0e3ae9c82
Matthew Davis <matthew@appsoa.io>
2016-08-10 19:34:38 +0000

    initial commit

---
+++ src/server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+
+var PORT = 8080;
+var app = express();
+
+app.get('/', function (req, res){
+    
+  res.send('Hello world!');
+  
+});
+
+app.listen(PORT);
+
+console.log('Running on http://localhost:' + PORT);
